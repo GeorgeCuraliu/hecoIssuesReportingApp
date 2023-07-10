cbbee2a8251f938280cd3e51f7560a85e3038361
GeorgeCuraliu <120975175+GeorgeCuraliu@users.noreply.github.com>
2023-07-11 00:52:09 +0300

    test push

--- server.js
+++ server.js
@@ -32,6 +32,34 @@ function verifyCredentials(username, password) {
     });
 }
 
+function verifyAndUpdateCredentials(username, password, newUsername) {
+    return new Promise((resolve, reject) => {
+      // Check if the credentials are valid
+      const query = 'SELECT * FROM users WHERE username = ? AND password = ?';
+      db.get(query, [username, password], (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          if (row) {
+            // Credentials are valid, update the username
+            const updateQuery = 'UPDATE users SET username = ? WHERE username = ?';
+            db.run(updateQuery, [newUsername, username], (err) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(true); // Update successful
+              }
+            });
+          } else {
+            resolve(false); // Invalid credentials
+          }
+        }
+      });
+    });
+  }
+  
+
+
 // Usage example
 const var1 = 'user1';
 const var2 = 'pass1';
@@ -76,6 +104,25 @@ verifyCredentials(var1, var2)
 //              -> LIKE	    returns 1 if a value matches a pattern
 //              -> NOT	    reverses the value of other operators such as NOT EXISTS, NOT IN, NOT BETWEEN, etc
 //              -> OR	    returns true if either expression is 1
-// where albumID = 1 (row - logical operaor)
+// WHERE albumID = 1    (row - logical operaor)
+//LIMIT numb OFFSET num     (limits the returning values to numb -- optional -> OFFSET (will offset the first returning value by numv values))
+//(NOT) BETWEEN 14.91 and 18.86   (used for row values)
+//MediaTypeId IN (1, 2)     (checks if the MediaTypeID value are 1 or 2)
+//INSERT INTO table (column1,column2 ,..) VALUES( value1,	value2 ,...);      column -> value
+//
+//
+
+
+
+
+
+
+
+//
+//https://www.sqlitetutorial.net/sqlite-insert/
+//https://www.codecademy.com/learn/learn-node-sqlite/modules/learn-node-sqlite-module/cheatsheet
+//https://github.com/TryGhost/node-sqlite3/wiki/Control-Flow
+//
+
 
                          
