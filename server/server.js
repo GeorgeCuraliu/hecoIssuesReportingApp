50c72e0779b03b1a92d7c39fbb4f8feeac20fcf7
GeorgeCuraliu <georgecuraliu@gmail.com>
2023-07-13 14:49:33 +0300

    updated server

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const path = './db/hecoAppDB.db';
 const db = new sqlite3.Database(path);
 
 
-const createAccount = (req) => {//req.body.username  req.body.password   req.body.name   req.body.gmail
+app.post("/createAccount", (req, res) => {//req.body.username  req.body.password   req.body.name   req.body.gmail
     let query = `SELECT * FROM users WHERE username = ?`;
         db.get(query, [req.body.username], (err, row) => {
             if(err){return (err)}
@@ -20,76 +20,90 @@ const createAccount = (req) => {//req.body.username  req.body.password   req.bod
             if(!row){
                 db.run(`INSERT INTO users(username, password, name, gmail) VALUES ("${req.body.username}", "${req.body.password}", "${req.body.name}", "${req.body.gmail}")`)
                 console.log("created account");
+                return res.status(200).send()
             }else{
                 console.log("the username is already used")
+                return res.status(203).send("user already exists")
             }
     })
-}
+})
 
-const deleteUser = (req) => {//req.body.username
+app.post("/deleteUser", (req, res) => {//req.body.username
 
     let query = `DELETE FROM users WHERE username = ?`;
-    db.run(query, req.body.username, () => {console.log("user deleted")})
+    db.run(query, req.body.username, (err) => {
+        console.log("user deleted")
+        return res.status(200).send()
+    })
 
-}
 
-const logIn = (req) => {//req.body.username  req.body.password
+})
+
+app.post("/logIn", (req, res) => {//req.body.username  req.body.password
 
     let query = `SELECT * FROM users WHERE username = ? AND password = ?`;
         db.get(query, [req.body.username, req.body.password], (err, row) => {
             if (err) {
                 console.log(err);
+                return res.status(400).send()
             } else {
                 console.log(!!row); // Resolves to true if row exists, false otherwise
+                return res.status(200).send(!!row)
             }
     });
 
-}
+})
 
 
-const createMachine = (req) => {//req.body.code  req.body.hangar  req.body.maintenance
+app.post("/createMachine", (req, res) => {//req.body.code  req.body.hangar  req.body.maintenance
 
-    db.run(`INSERT INTO machineRegistry(code, hangar, maintenance, status) VALUES ("${req.body.code}", "${req.body.hangar}", "${req.body.maintenance}", "functional")`)
-    console.log("Added machineRegistry to server")
+    db.run(`INSERT INTO machineRegistry(code, hangar, maintenance, status) VALUES ("${req.body.code}", "${req.body.hangar}", "${req.body.maintenance}", "functional")`, err => {
+        console.log("Added machineRegistry to server")
+        return res.status(200).send();
+    })
 
-}
+})
 
-const deleteMachine = (req) => {//req.body.code
+app.post("/deleteMachine", (req, res) => {//req.body.code
 
-    db.run(`DELETE FROM machineRegistry WHERE code = "${req.body.code}"`)
+    db.run(`DELETE FROM machineRegistry WHERE code = "${req.body.code}"`, err => {
+        return res.status(200);
+    })
 
-}
+})
 
-const getMachines = (req) => {
+app.post("/getMachines", (req) => {
 
     let query = "SELECT * FROM machineRegistry";
     db.all(query, [], (err, rows) => {
         if(err){
-            console.log("big error")
+            return res.status(404)
         }else{
-            console.log(rows);
+            return res.status(200).send(rows);
         }
     })
 
-}
+})
 
 
 
 
-const createRepairRequest = (req) => {//req.body.machineCode  req.body.by  req.body.targetGroup  req.body.issue
+app.post("/createRepairRequest", (req, res) => {//req.body.machineCode  req.body.by  req.body.targetGroup  req.body.issue
 
     db.get("SELECT * FROM repairRequests WHERE machineCode = ?", [req.body.machineCode], (err, row) => {
-        if(err){return "no no"}
-        if(row){return "no no"}
+        if(err){return res.status(404)}
+        if(row){return res.status(404)}
         db.run(`INSERT INTO repairRequests(by, targetGroup, machineCode, issue) VALUES ("${req.body.by}", "${req.body.targetGroup}", "${req.body.machineCode}", "${req.body.issue}")`, err => {
-            if(err){return}
+            if(err){return res.status(404)}
 
-            db.run(`UPDATE machineRegistry SET status = "in repairing" WHERE code = "${req.body.machineCode}"`)
+            db.run(`UPDATE machineRegistry SET status = "in repairing" WHERE code = "${req.body.machineCode}"`, (err) => {
+                return res.status(200).send()
+            })
         });
         
     })
 
-}
+})
 
 
 
